d3f3084e7ae1decc14904797277f9b2f21b34fe6
apkanagesh <apkanagesh@gmail.com>
2024-09-30 21:20:49 +0100

    variables

--- 16-redirectors.sh
+++ 16-redirectors.sh
@@ -14,7 +14,7 @@ Y="\e[33m"
 
 
 CHECK_ROOT(){
-    if [ $USERID -ne 0]
+    if [ $USERID -ne 0 ]
     then
         echo -e "$R please run this script with root preveleges $N" &>>LOG_FILE
         exit 1
